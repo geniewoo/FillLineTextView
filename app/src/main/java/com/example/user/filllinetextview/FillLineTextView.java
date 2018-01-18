488ce0df8bc65712a54e729fab08222fee59655d
Sungwoo_Park <mandoo992@naver.com>
2018-01-18 11:16:01 +0900

    Initial commit

---
+++ app/src/main/java/com/example/user/filllinetextview/FillLineTextView.java
@@ -0,0 +1,24 @@
+package com.example.user.filllinetextview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by USER on 2018-01-18.
+ */
+
+public class FillLineTextView extends TextView {
+    public FillLineTextView(Context context) {
+        super(context);
+    }
+
+    public FillLineTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FillLineTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
